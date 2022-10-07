af790fb8cdd65d0112f96dc3f12436728090caef
Patrick Millerson <patmillerson@gmail.com>
2022-10-07 01:35:31 -0400

    init

--- app/settings.py
+++ app/settings.py
@@ -23,8 +23,27 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-07(v#7dy&lcuwb7spl*iidd^@n#^v)8m1jbvzvcf)$+idm9!4h'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('DJANGO_DEVELOPMENT') == "true":
+    DEBUG = True
+    STATIC_URL = 'static'
+
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+
+
+    STATIC_ROOT = 'staticfiles'
+    
+else:
+    DEBUG = False
+    STATIC_URL = '/tmp/build/staticfiles'
+
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+
+
+    STATIC_ROOT = 'staticfiles'
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,14 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-
-STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
